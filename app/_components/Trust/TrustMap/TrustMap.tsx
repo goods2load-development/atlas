ed3488d89c4a4251fcc653cb1754f747b6b3cf9b
CodeJoker <126978312+RuntimeTerror505@users.noreply.github.com>
2024-05-13 14:37:09 +0300

    fix build

--- app/_components/Trust/TrustMap/TrustMap.tsx
+++ app/_components/Trust/TrustMap/TrustMap.tsx
@@ -14,7 +14,7 @@ const TrustMap = () => {
       </h1>
       <Image className="w-full" src={map} alt={"map"} />
       <p className="text-black text-center px-[180px] font-normal text-[18px]/[25px]">
-        With GOODS2LOAD, the world is your oyster. Our commitment to providing
+        {`With GOODS2LOAD, the world is your oyster. Our commitment to providing
         comprehensive coverage across global routes means that your ideas are
         never constrained by geographic limitations. Whether you're envisioning
         expansion into new markets, sourcing materials from distant suppliers,
@@ -22,7 +22,7 @@ const TrustMap = () => {
         empowers you to dream big and reach far. With GOODS2LOAD, you have the
         freedom to explore endless possibilities and unlock the full potential
         of your business, knowing that we have you covered every step of the
-        way.
+        way.`}
       </p>
     </section>
   );
