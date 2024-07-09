a9d5044c2c516d2239a41ee92ab2a57826b4d5ff
ProgrammingBreaker <volodymyr.vovklv@gmail.com>
2024-07-09 15:29:50 +0300

    fixes

--- components/SelectedSearch.tsx
+++ components/SelectedSearch.tsx
@@ -3,9 +3,12 @@
 import { format } from "date-fns";
 import { useFilterStore } from "@/lib/filterStore";
 import { number } from "zod";
+import { useEffect } from "react";
+import Image from "next/image";
 
 export default function SelectedSearch() {
   const {
+    deliveryBy,
     fromCountry,
     from,
     toCountry,
@@ -26,99 +29,157 @@ export default function SelectedSearch() {
     return width * height * length;
   };
 
+  const renderIcon = (deliveryBy: string) => {
+    switch (deliveryBy) {
+      case "plane": {
+        return (
+          <Image
+            src={`/filtericon-plane.svg`}
+            alt={"plane"}
+            width={58}
+            height={58}
+          />
+        );
+      }
+      case "ferry": {
+        return (
+          <Image
+            src={`/filtericon-ship.svg`}
+            alt={"plane"}
+            width={58}
+            height={58}
+          />
+        );
+      }
+      case "truck": {
+        return (
+          <Image
+            src={`/filtericon-truck.svg`}
+            alt={"plane"}
+            width={58}
+            height={58}
+          />
+        );
+      }
+    }
+  };
+
   return (
     <div
-      className={`bg-[#ffede4] rounded-xl font-bold text-[16px]/[20px] text-[#ff6720] items-end sm:p-[24px] p-4 px-1 mt-[10px] relative z-50`}
+      className={`bg-[#ffede4] rounded-xl font-bold text-[16px]/[20px] text-[#ff6720] sm:p-[24px] p-4 px-1 mt-[10px] relative z-50 max-w-[1296px] xl:mx-auto`}
     >
-      <div className="flex justify-stretch items-end w-full overflow-x-scroll pb-4">
+      <div className="flex lg:justify-start justify-center xl:justify-center items-center overflow-x-scroll pb-4 hide-scrollbar">
         <div className="flex flex-wrap lg:flex-nowrap items-end justify-center lg:justify-start">
+          <div className="lg:block hidden mt-4 mr-2">
+            {renderIcon(deliveryBy)}
+          </div>
           {fromCountry && from && (
-            <div className="mr-[1px] w-[45%] lg:w-auto mb-4 lg:mb-0">
-              <label className="mb-2 block">From</label>
-              <div className="h-[60px] rounded-l-[16px] border-none font-normal text-black w-full justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm">
+            <div className="mr-[1px] w-[45%] lg:w-[134px] mb-4 lg:mb-0">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                From
+              </label>
+              <div className="h-[60px] rounded-l-[16px] border-none font-normal text-black justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm overflow-scroll hide-scrollbar">
                 {fromCountry}, {from}
               </div>
             </div>
           )}
           {toCountry && to && (
-            <div className="sm:mr-[1px] w-[45%] lg:w-auto mb-4 lg:mb-0">
-              <label className="mb-2 block">To</label>
-              <div className="h-[60px] border-none rounded-r-[16px] lg:rounded-none font-normal text-black w-full justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm">
+            <div className="sm:mr-[1px] w-[45%] lg:w-[126px] mb-4 lg:mb-0">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                To
+              </label>
+              <div className="h-[60px] border-none rounded-r-[16px] lg:rounded-none font-normal text-black justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm overflow-scroll hide-scrollbar">
                 {toCountry}, {to}
               </div>
             </div>
           )}
           {departure && (
-            <div className="mr-[1px] w-[45%] lg:w-auto mb-4 lg:mb-0">
-              <label className="mb-2 block">Departure</label>
-              <div className="h-[60px] rounded-l-[16px] lg:rounded-l-none border-none font-normal text-black w-full justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm">
+            <div className="mr-[1px] w-[45%] lg:w-[128px] mb-4 lg:mb-0">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                Departure
+              </label>
+              <div className="h-[60px] rounded-l-[16px] lg:rounded-l-none border-none font-normal text-black justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm">
                 {format(departure, "dd/MM/yyyy")}
               </div>
             </div>
           )}
           {arrival && (
-            <div className="mr-[1px] w-[45%] lg:w-auto mb-4 lg:mb-0">
-              <label className="mb-2 block">Arrival</label>
-              <div className="h-[60px] lg:rounded-r-none border-none font-normal rounded-r-[16px] text-black w-full justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm">
+            <div className="mr-[1px] w-[45%] lg:w-[128px] mb-4 lg:mb-0">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                Arrival
+              </label>
+              <div className="h-[60px] lg:rounded-r-none border-none font-normal rounded-r-[16px] text-black justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm">
                 {format(arrival, "dd/MM/yyyy")}
               </div>
             </div>
           )}
           {typeOfGoods && (
-            <div className="mr-[1px] w-[30%] lg:w-auto mb-4 lg:mb-0">
-              <label className="mb-2 block">Goods</label>
-              <div className="h-[60px]  w-full rounded-l-[16px] lg:rounded-l-none border-none font-normal text-black  justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm pr-2">
-                <div className="w-[100px] h-full flex items-center overflow-x-scroll">
+            <div className="mr-[1px] w-[30%] lg:w-[122px] mb-4 lg:mb-0">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                Goods
+              </label>
+              <div className="h-[60px] rounded-l-[16px] lg:rounded-l-none border-none font-normal text-black  justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm pr-2">
+                <div className="h-full flex items-center overflow-x-scroll hide-scrollbar max-w-[122px] sm:max-w-auto">
                   {typeOfGoods}
                 </div>
               </div>
             </div>
           )}
           {placementOfGoods && (
-            <div className="mr-[1px] w-[30%] lg:w-auto mb-4 lg:mb-0">
-              <label className="mb-2 block">Placement</label>
-              <div className="h-[60px]  w-full rounded-r-none border-none font-normal text-black  justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm pr-2">
-                <div className="w-[100px] h-full flex items-center overflow-x-scroll">
+            <div className="mr-[1px] w-[30%] lg:w-[122px] mb-4 lg:mb-0">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                Placement
+              </label>
+              <div className="h-[60px] w-full rounded-r-none border-none font-normal text-black  justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm pr-2">
+                <div className="h-full flex items-center overflow-x-scroll hide-scrollbar">
                   {placementOfGoods}
                 </div>
               </div>
             </div>
           )}
           {width && length && height && (
-            <div className="mr-[1px] w-[30%] lg:w-auto mb-4 lg:mb-0">
-              <label className="mb-2 block">Volume</label>
+            <div className="mr-[1px] w-[30%] lg:w-[154px] mb-4 lg:mb-0">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                Volume
+              </label>
               <div className="h-[60px]  w-full rounded-r-[16px] lg:rounded-r-none border-none font-normal text-black  justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm pr-2">
-                <div className="w-[100px] h-full flex items-center overflow-x-scroll">
+                <div className=" h-full flex items-center overflow-x-scroll hide-scrollbar">
                   Vol: {countVolume(width, length, height)} m³
                 </div>
               </div>
             </div>
           )}
           {quantity && (
-            <div className="mr-[1px] w-[30%] lg:w-auto">
-              <label className="mb-2 block">Quantity</label>
+            <div className="mr-[1px] w-[30%] lg:w-[87px]">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                Quantity
+              </label>
               <div className="h-[60px]  w-full rounded-l-[16px] lg:rounded-l-none border-none font-normal text-black  justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm pr-2">
-                <div className="w-[100px] h-full flex items-center overflow-x-scroll">
+                <div className=" h-ful flex items-center overflow-x-scroll hide-scrollbar">
                   {quantity}
                 </div>
               </div>
             </div>
           )}
           {goodsValue && (
-            <div className="mr-[1px] w-[30%] lg:w-auto">
-              <label className="mb-2 block">Goods Value</label>
+            <div className="mr-[1px] w-[30%] lg:w-[112px]">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                Goods Value
+              </label>
               <div className="h-[60px]  w-full rounded-r-none border-none font-normal text-black  justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm pr-2">
-                <div className="w-[100px] h-full flex items-center overflow-x-scroll">
+                <div className="h-full flex items-center overflow-x-scroll hide-scrollbar">
                   $ {goodsValue}
                 </div>
               </div>
             </div>
           )}
           {incoterms && (
-            <div className="mr-[1px] w-[30%] lg:w-auto">
-              <label className="mb-2 block">Incoterms</label>
+            <div className="mr-[1px] w-[30%] lg:w-[85px]">
+              <label className="mb-2 block text-[15px]/[22px] font-semibold">
+                Incoterms
+              </label>
               <div className="h-[60px] rounded-r-[16px]  w-full  border-none font-normal text-black  justify-start bg-white flex items-center whitespace-nowrap px-[16px] text-sm pr-2">
-                <div className="w-[100px] h-full flex items-center overflow-x-scroll">
+                <div className=" h-full flex items-center overflow-x-scroll hide-scrollbar">
                   {incoterms}
                 </div>
               </div>
