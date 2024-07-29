76707a3a1f6afab209e38f4e2025286774634329
RuntimeTerror505 <andriyilkiv322@gmail.com>
2024-07-29 17:55:24 +0300

    select changed to combobox

--- components/common/CountryCode.tsx
+++ components/common/CountryCode.tsx
@@ -1,45 +1,72 @@
 "use client";
+import React, { useState } from "react";
 import CountryList from "country-list-with-dial-code-and-flag";
+import { Button } from "@/components/ui/button";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function CountryCode(props: any) {
   const phoneCodes = CountryList.getAll();
-  // TODO codes sorting
-  // .sort((a, b) => {
-  //   if (a.countryCode < b.countryCode) {
-  //     return -1;
-  //   } else if (a.countryCode >   b.countryCode) {
-  //     return 1;
-  //   }
-  //   return 0;
-  // });
-
-  // TODO change for combobox
+  function filter(value: string, search: string) {
+    if (value.includes(search.toLocaleLowerCase())) return 1;
+    else return 0;
+  }
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState<any>(undefined);
   return (
-    <Select
-      onValueChange={props.onChange}
-      defaultValue={phoneCodes[0].countryCode}
-    >
-      <SelectTrigger className={props.className}>
-        <SelectValue />
-      </SelectTrigger>
-      <SelectContent>
-        {phoneCodes.map((item: any, index: number) => (
-          <SelectItem
-            key={`${item.countryCode}-${index}`}
-            value={item.countryCode}
-          >
-            {item.flag}
-            {item.countryCode}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <Popover open={open}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          className={props.className + " w-full"}
+          onClick={() => setOpen(true)}
+        >
+          {selected ? (
+            <span className="block w-full truncate">
+              {selected.flag}
+              {selected.countryCode}
+            </span>
+          ) : (
+            <span className="text-gray-500">Select</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent side="bottom" className="w-full p-0">
+        <Command filter={filter}>
+          <CommandInput placeholder="Search..." />
+          <CommandEmpty>Not found.</CommandEmpty>
+          <ScrollArea className="h-72 w-full">
+            <CommandGroup>
+              {phoneCodes.map((item: any, index: number) => (
+                <CommandItem
+                  value={`${item.countryCode}`}
+                  key={index}
+                  onSelect={() => {
+                    props.onChange(item.countryCode);
+                    setSelected(item);
+                    setOpen(false);
+                  }}
+                >
+                  {item.flag}
+                  {item.countryCode}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </ScrollArea>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
 }
