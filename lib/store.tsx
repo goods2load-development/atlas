31008d0dc9ac7cff297fb1c28a4c57576a37db3a
ProgrammingBreaker <volodymyr.vovklv@gmail.com>
2024-08-22 10:16:44 +0300

    upload company logo

--- lib/store.tsx
+++ lib/store.tsx
@@ -96,6 +96,7 @@ export const useRegistrationStore = create((set) => ({
     formData.append("insuranceStatement", data.insuranceStatement);
     formData.append("issuingAuthority", data.issuingAuthority);
     formData.append("tradeLicenseNumber", data.tradeLicenseNumber);
+    formData.append("companyPhoto", data.companyPhoto);
 
     delete data.confirmPassword;
     delete data.privacy;
@@ -103,6 +104,7 @@ export const useRegistrationStore = create((set) => ({
     delete data.insuranceStatement;
     delete data.issuingAuthority;
     delete data.tradeLicenseNumber;
+    delete data.companyPhoto;
 
     delete data.license;
     postRequest({
