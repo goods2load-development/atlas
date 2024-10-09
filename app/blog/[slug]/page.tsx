f61b4291bc325d0f04bc36b8e50251479f0d7026
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2024-10-09 16:25:33 +0200

    feat: Add seo links to blog and template pages.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,26 +1,48 @@
+import { Metadata } from "next";
+import { headers } from "next/headers";
 import BlogSlug from "@/components/BlogSlug/BlogSlug";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header/Header";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Blog",
-};
+async function getBlogData(slug: string) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}api/blogs/slug/${slug}`,
+    {
+      cache: "no-store",
+    }
+  );
+  return response.json();
+}
+
+export async function generateMetadata({
+  params: { slug },
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const blog = await getBlogData(slug);
+  const headersList = headers();
+  const host = headersList.get("host");
+  const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+
+  const canonical = `${protocol}://${host}/blog/${slug}`;
+
+  return {
+    title: blog.title,
+    description: blog.description,
+    authors: [{ name: blog.authorName }],
+    keywords: blog.blogTypeName?.split(", "),
+    alternates: {
+      canonical,
+    },
+  };
+}
 
 export default async function BlogPage({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
-  const [blog] = await Promise.all([
-    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/blogs/slug/${slug}`, {
-      cache: "no-store",
-    }).then((res) => res.json()),
-  ]);
-
-  metadata.title = blog.title;
-  metadata.description = blog.description;
-  metadata.authors = blog.authorName;
+  const blog = await getBlogData(slug);
 
   return (
     <>
--- components/SeoPage/SeoPageMain.tsx
+++ components/SeoPage/SeoPageMain.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Image from "next/image";
 
 import useDotButton from "@/app/hooks/useDotButton";
