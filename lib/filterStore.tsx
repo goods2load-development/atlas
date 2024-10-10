19e0877826a96202b7ce4e78e729e71cfcfcac6e
DumitruButnari <dimkalanu@gmail.com>
2024-10-10 13:13:28 +0300

    refactor price_alert into solution finder
    fix mobile issues on consent popup

--- lib/filterStore.tsx
+++ lib/filterStore.tsx
@@ -141,7 +141,7 @@ export const useFilterStore = create<FilterStoreProps>((set, get) => {
           height,
           goodsValue,
         },
-        newFilter
+        newFilter,
       );
       set((state: FilterStoreProps) => ({
         ...state,
@@ -173,7 +173,7 @@ export const useFilterStore = create<FilterStoreProps>((set, get) => {
               label: item.properties.name,
             }));
             const selected: any[] = data?.features.map(
-              (item: any) => item.properties.name
+              (item: any) => item.properties.name,
             );
             if (departure) {
               set(() => ({
@@ -236,7 +236,7 @@ export const useFilterStore = create<FilterStoreProps>((set, get) => {
           height,
           goodsValue,
           incoterms,
-        })
+        }),
       );
 
       postRequest({
@@ -299,20 +299,19 @@ export const useFilterStore = create<FilterStoreProps>((set, get) => {
           },
           withdraw: format(
             new Date(item.withdraw).toDateString(),
-            "MM/dd/yyyy"
+            "MM/dd/yyyy",
           ),
           delivery: format(
             new Date(item.delivery).toDateString(),
-            "MM/dd/yyyy"
+            "MM/dd/yyyy",
           ),
           orderCost: item.price,
           CO2EmissionControlled: item.goGreen,
           portArrival: item.portArrival,
           portDeparture: item.portDeparture,
           price: item.price, // Added for analytics avarge store when user select this product
-          placementOfGoods: item.placementOfGoods // Added for analytics avarge store when user select this product
+          placementOfGoods: item.placementOfGoods, // Added for analytics avarge store when user select this product
         }));
-        console.log("products", products);
         set(() => ({ products, pagination: data?.partners?.meta }));
       });
     },
@@ -360,7 +359,7 @@ export const useCurrenciesStore = create<CurrenciesStoreProps>((set, get) => ({
         }
       });
       const majorCurrencies = currenciesSorted.filter(
-        (i: any) => i.code === "USD" || i.code === "EUR" || i.code === "GBP"
+        (i: any) => i.code === "USD" || i.code === "EUR" || i.code === "GBP",
       );
       set(() => ({
         currencies: majorCurrencies
@@ -368,8 +367,8 @@ export const useCurrenciesStore = create<CurrenciesStoreProps>((set, get) => ({
             currenciesSorted.filter(
               (i: any) =>
                 !(i.code === "USD" || i.code === "EUR" || i.code === "GBP") &&
-                exchangeRates[i.code]
-            )
+                exchangeRates[i.code],
+            ),
           )
           .map((item: any) => ({ ...item, rate: exchangeRates[item.code] })),
         selectedCurrency: {
