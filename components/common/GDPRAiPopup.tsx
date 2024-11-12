fa2549182de09dbb3b7eafdcfa2a55e672d974d5
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2024-11-12 13:34:55 +0100

    feat: Added icons and links go gdpra ai popupp.

--- components/common/GDPRAiPopup.tsx
+++ components/common/GDPRAiPopup.tsx
@@ -38,19 +38,36 @@ const GDPRAiPopup = () => {
     <Dialog open={isVisible}>
       <DialogContent className="max-w-4xl p-4 md:p-12 md:gap-9 w-[95%] max-h-[90vh] overflow-y-auto">
         <DialogHeader className="flex flex-row items-center mx-auto gap-4">
-          <Image src={CookiesImage} alt="Cookies" width={48} height={48} />
+          <Image src="/ai.svg" alt="AI" width={64} height={64} />
           <DialogTitle className="text-2xl md:text-5xl font-thin">
-            AI Usage <span className="font-normal italic">and</span> Disclaimer
-            Notice
+            <span className="font-normal italic">AI Usage</span> and{' '}
+            <span className="font-normal italic">Disclaimer Notice</span>
           </DialogTitle>
         </DialogHeader>
         <DialogDescription className="text-center text-blackTertiary text-[14px] md:text-base">
           By choosing to upload an image and use the image search feature, you
-          agree to the Terms of Service and that your data will be processed
-          according to our Privacy Policy. You agree that your images may be
-          stored and used to improve website functionalities, and may also be
-          forwarded to Transportation Providers. You must not upload any
-          personal photos or personally identifiable images.
+          agree to the{' '}
+          <a
+            href="/terms-of-service"
+            target="_blank"
+            rel="noreferrer"
+            className="underline"
+          >
+            Terms of Service
+          </a>{' '}
+          and that your data will be processed according to our{' '}
+          <a
+            href="/privacy-policy"
+            target="_blank"
+            rel="noreferrer"
+            className="underline"
+          >
+            Privacy Policy
+          </a>
+          . You agree that your images may be stored and used to improve website
+          functionalities, and may also be forwarded to Transportation
+          Providers. You must not upload any personal photos or personally
+          identifiable images.
         </DialogDescription>
         <DialogFooter className="flex sm:justify-center md:gap-10 gap-3">
           <UIButton
