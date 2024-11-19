a6b46cf8a89ed0d75537e7bcf3f6472106e5bd81
ProgrammingBreaker <volodymyr.vovklv@gmail.com>
2024-11-19 18:36:52 +0200

    fix: blog title text, slider, according structure

--- app/_components/Help/HelpAccordion/HelpAccordion.tsx
+++ app/_components/Help/HelpAccordion/HelpAccordion.tsx
@@ -23,11 +23,11 @@ const HelpAccordion: React.FC<HelpAccordion> = ({ curAnswearCondition }) => {
   return (
     <div className="w-full pb-[104px] px-4  max-w-[990px] flex flex-col justify-center items-center sm:mt-52 mt-40">
       <div className="w-full flex justify-center items-center flex-col">
-        <div className="text-center text-black text-[34px]/[38px] sm:text-[48px]/[76px] mb-[40px]">
+        <h2 className="text-center text-black text-[34px]/[38px] sm:text-[48px]/[76px] mb-[40px]">
           <i className="bg-allTittleColor px-2 rounded-md">Answers</i>
           <span> to Your Burning </span>
           <i className="bg-allTittleColor px-2 rounded-md">Questions</i>
-        </div>
+        </h2>
 
         <Accordion
           type="single"
@@ -57,14 +57,14 @@ const HelpAccordion: React.FC<HelpAccordion> = ({ curAnswearCondition }) => {
                         <i className="text-[20px]/[24px] sm:text-[24px]/[28px] font-light sm:font-normal pr-4 pb-8 sm:pr-[40px] sm:pb-[30px] sm:p-0">
                           0{index + 1}
                         </i>
-                        <span className="text-black text-left pb-6 sm:pb-[30px] pr-5 max-w-[85%]">
+                        <h3 className="text-black text-left pb-6 sm:pb-[30px] pr-5 max-w-[85%]">
                           {question.toUpperCase()}
                           <div
                             className={`absolute right-8 sm:right-[50px] top-2 transform translate-x-[30px] transition duration-300 ${open ? '-scale-y-100' : ''}`}
                           >
                             <HelpArrowAccordionSvg />
                           </div>
-                        </span>
+                        </h3>
                       </div>
                     </AccordionTrigger>
                     <AccordionContent className="text-black text-left sm:pl-[60px] pl-8 text-[15px]/[22px] font-light sm:text-[16px]/[24px]">
