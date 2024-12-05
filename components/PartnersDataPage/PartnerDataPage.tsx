53d24b6c3e20ac4212ebe2112df1d65c51fde567
DumitruButnari <dimkalanu@gmail.com>
2024-12-05 16:08:53 +0200

    rename partners charts

--- components/PartnersDataPage/PartnerDataPage.tsx
+++ components/PartnersDataPage/PartnerDataPage.tsx
@@ -767,7 +767,7 @@ const PartnerDataPage = ({
 
                       <div className="mt-[30px] flex-1">
                         <div className="font-medium border-b border-lightOrange pb-2 w-full mb-6">
-                          Country focus
+                          Countries focus
                         </div>
                         {!isGet && (
                           <>
@@ -873,7 +873,7 @@ const PartnerDataPage = ({
 
                       <div className="mt-[30px] flex-1">
                         <div className="font-medium border-b border-lightOrange pb-2 w-full mb-6">
-                          Service lines
+                          Industries we serve
                         </div>
                         {!isGet && (
                           <>
@@ -975,7 +975,7 @@ const PartnerDataPage = ({
 
                       <div className="mt-[30px] flex-1">
                         <div className="font-medium border-b border-lightOrange pb-2 w-full mb-6">
-                          Services lines
+                          Our values
                         </div>
 
                         {!isGet && (
@@ -1156,7 +1156,7 @@ const PartnerDataPage = ({
             </>
           )}
 
-          <div className="mt-10 md:mt-[112px]">
+          <div id="awards" className="mt-10 md:mt-[112px]">
             <h3 className="text-4xl md:text-[48px] mb-8 text-black text-center md:text-left">
               <div className="bg-[#FEF1DF] font-light p-1 rounded-sm inline-block">
                 <span>Awarded</span>
