7a4bc89f43521ab641a1fca93b992c878c6bf420
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2024-12-20 13:44:37 +0100

    feat: Connected bing webmaster and microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import WhatsupIcon from '@/assets/icons/whatsupicon.svg';
 import { generateDefaultMetadata } from '@/lib/metadataUtils';
 import GtagProvider from '@/lib/providers/GtagProvider';
 
-// import { GoogleTagManager } from '@next/third-parties/google';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import Image from 'next/image';
@@ -24,6 +23,7 @@ const poppins = Poppins({
 export function generateMetadata(): Metadata {
   return generateDefaultMetadata();
 }
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -38,6 +38,24 @@ export default function RootLayout({
           <meta
             name="google-site-verification"
             content="DhV93E_5VWkiNA1ic5_JkLvLifE-XJVbq5uOwE4TkE8"
+          />
+
+          <meta
+            name="msvalidate.01"
+            content="F7DB9475600B7983F23BF99A0B1D3081"
+          />
+
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "pgv2b4qmpq");
+              `,
+            }}
           />
         </head>
         <body className={poppins.className}>
@@ -55,6 +73,7 @@ export default function RootLayout({
             href="https://wa.me/+971505574291"
             className="sm:pl-[20px] text-[14px]/[16px] bg-white rounded-full flex flex-row border-[1px] shadow-lg border-white fixed z-50 bottom-[16px] sm:bottom-[40px] right-[16px] sm:right-[40px]"
             target="_blank"
+            rel="noopener noreferrer"
           >
             <span className="hidden sm:inline self-center pr-[10px]">
               Chat by WhatsApp
